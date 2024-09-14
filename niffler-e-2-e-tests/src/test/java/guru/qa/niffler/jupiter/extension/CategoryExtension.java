a7f6129f6f1edd588d29fd1bc5b37e9e93920a6a
EpicFate <bogtinys20@gmail.com>
2024-09-14 17:17:11 +0300

    resolve conflict

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/CategoryExtension.java
@@ -2,6 +2,7 @@ package guru.qa.niffler.jupiter.Category;
 
 import com.github.javafaker.Faker;
 import guru.qa.niffler.api.SpendApiClient;
+import guru.qa.niffler.jupiter.annotation.Category;
 import guru.qa.niffler.model.CategoryJson;
 import org.junit.jupiter.api.extension.*;
 import org.junit.platform.commons.support.AnnotationSupport;
